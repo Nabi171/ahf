8f5304bf7c01a2929eee36760d157d8ac420953a
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-10-28 11:44:56 +0600

    Added

---
+++ SynAsyn.js
@@ -0,0 +1,18 @@
+// const colors = require("colors");
+
+console.log('test the procedure');
+
+setTimeout(() => {
+    const user = { id: 1 };
+    setTimeout(() => {
+        const products = [{}, {}, {}];
+        setTimeout(() => {
+            const prices = [];
+        }, 1000);
+    }, 500)
+
+
+
+}, 1000);
+
+console.log('okay start');
