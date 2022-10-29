10bcdbdc004afeb5a7167a08c0d71d9f58ecce3f
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-10-29 11:47:12 +0600

    updated

--- Promise.js
+++ Promise.js
@@ -4,7 +4,7 @@ const myPromise = new Promise((resolve, reject) => {
         reject("something went wrong!");
     } else {
         setTimeout(() => {
-            resolve("successfully got the data")
+            resolve({ name: "John" })
         }, 1000)
     }
 
@@ -13,6 +13,18 @@ const myPromise = new Promise((resolve, reject) => {
 
 
 });
+const userIds = [1, 2, 3, 4, 5];
+let userData = [];
+
+for (let i = 0; i < userIds.length; i++) {
+    const userId = userIds[i];
+    userData.push(myPromise);
+}
+
+Promise.all(userData).then(res => {
+    console.log(res);
+})
+console.log(userData);
 
 myPromise.then(res => console.log(res))
     .catch(err => console.log(err));
