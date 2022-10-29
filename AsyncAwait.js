563b2174d231cecd48fc407f8605cb5ff9fdec45
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-10-29 12:02:19 +0600

    async added

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -13,3 +13,16 @@ async function getData() {
 }
 
 getData();
+
+fetch(""), then(res => res.json()).then(data => console.log(data)).catch(err => console.log(err));
+
+
+async function getUser() {
+    const res = await fetch("");
+    const data = await res.json();
+
+    fetch(`https://www.example.com/${user.id}`);
+    const products = res2.json();
+
+    console.log(products);
+}
